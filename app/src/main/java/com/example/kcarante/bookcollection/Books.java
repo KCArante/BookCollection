2354bce8e38f7e75ed78eb96c6ae6f4354bfffd4
KCArante <kc.arante@gmail.com>
2016-02-23 17:28:48 -0800

    Initial commit

---
+++ app/src/main/java/com/example/kcarante/bookcollection/Books.java
@@ -0,0 +1,63 @@
+package com.example.kcarante.bookcollection;
+
+/**
+ * Created by SARJ on 2/23/2016.
+ */
+public class Books {
+
+    private String id;
+    private String title;
+    private String genre;
+    private String author;
+    private boolean isRead;
+
+    public Books(){}
+
+    public Books(String id, String title, String genre, String author, boolean isRead) {
+        this.id = id;
+        this.title = title;
+        this.genre = genre;
+        this.author = author;
+        this.isRead = isRead;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public boolean isRead() {
+        return isRead;
+    }
+
+    public void setIsRead(boolean isRead) {
+        this.isRead = isRead;
+    }
+}
---
+++ app/src/main/java/com/example/kcarante/bookcollection/MainActivity.java
@@ -0,0 +1,129 @@
+package com.example.kcarante.bookcollection;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    //JSON Node names
+    private static final String TAG_ID = "_id";
+    private static final String TAG_TITLE = "title";
+    private static final String TAG_GENRE = "genre";
+    private static final String TAG_AUTHOR = "author";
+    private static final String TAG_ISREAD = "isRead";
+
+    //url
+    private static String url = "http://172.17.3.51:5856/api/books";
+
+    ArrayList<Books> b = new ArrayList<Books>();
+    JSONArray books = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        GetBooks task = new GetBooks();
+        task.execute();
+        ArrayAdapter<Books> adapter = new ArrayAdapter<>(this, R.layout.list_item, b);
+
+        ListView listView = (ListView) findViewById(android.R.id.list);
+        listView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private class GetBooks extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            HttpUtils httpUtils = new HttpUtils();
+
+            // Making a request to url and getting response
+            String jsonStr = httpUtils.getResponse(url, "");
+            Log.d("Response: ", "> " + jsonStr);
+
+            if(jsonStr != null){
+                try {
+                    JSONObject jsonObject = new JSONObject(jsonStr);
+                    books = jsonObject.getJSONArray("");
+
+                    for(int i=0; i<books.length(); i++){
+
+                        JSONObject samp = books.getJSONObject(i);
+                        Books bookSample = new Books();
+
+                        String id = samp.getString(TAG_ID);
+                        String title = samp.getString(TAG_TITLE);
+                        String genre = samp.getString(TAG_GENRE);
+                        String author = samp.getString(TAG_AUTHOR);
+                        boolean isRead = samp.getBoolean(TAG_ISREAD);
+
+                        bookSample.setId(id);
+                        bookSample.setTitle(title);
+                        bookSample.setGenre(genre);
+                        bookSample.setAuthor(author);
+                        bookSample.setIsRead(isRead);
+
+                        b.add(bookSample);
+                    }
+               }
+                catch(JSONException e){
+                    e.printStackTrace();
+                }
+            }
+            else {
+                Log.d("Server", "Error to connect on url");
+            }
+
+            return null;
+        }
+
+    }
+}
